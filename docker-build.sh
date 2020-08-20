206150577979062ccbd8e1ad30cda9392fbb12f4
Kaiyuan Fan <172881969@qq.com>
2020-08-19 21:07:55 -0400

    webssh refactor (#3)

--- docker-build.sh
+++ docker-build.sh
@@ -1,21 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+get_manifest_sha (){
+    local repo=$1     # <source>/alpine:latest
+    local arch=$2     # arm
+    docker pull -q $repo &>/dev/null
+    docker manifest inspect $repo > "$arch".txt
+    sha=""
+    local i=0
+    while [ "$sha" == "" ] && read -r line
+    do
+        archecture=$(jq .manifests[$i].platform.architecture "$arch".txt |sed -e 's/^"//' -e 's/"$//')
+        if [ "$archecture" = "$arch" ];then
+            sha=$(jq .manifests[$i].digest "$arch".txt  |sed -e 's/^"//' -e 's/"$//')
+            echo ${sha}
+        fi
+        i=$i+1
+    done < "$arch".txt
+}
 
 if [[ $# -le 1 ]]; then
     echo "missing parameters."
     exit 1
 fi
 
-dir=$(dirname $0)
-sha=$($dir/manifest-alpine-sha.sh $@)       # $1 <source>/alpine:latest  amd64|arm|arm64
-echo $sha
-base_image="treehouses/alpine@$sha"
-echo $base_image
-arch=$2   # arm arm64 amd64
+repo=$1
 
-if [ -n "$sha" ]; then
-        tag=treehouses/webssh-tags:$arch
-        echo $tag                           #treehouses/webssh-tags:arm
-        sed "s|{{base_image}}|$base_image|g" Dockerfile.template > Dockerfile.$arch
-        docker build -t $tag -f Dockerfile.$arch .
-        docker push $tag
-fi
+for i in ${@:2}
+do
+    sha=$(get_manifest_sha $repo $i)        #$1 treehouses/alpine:latest  amd64|arm|arm64
+    echo $sha
+    base_image="treehouses/alpine@$sha"
+    echo $base_image
+    arch=$i   # arm arm64 amd64
+
+    if [ -n "$sha" ]; then
+            tag_arch=treehouses/webssh-tags:$arch
+            echo $tag_arch                       #treehouses/webssh-tags:arm
+            sed "s|{{base_image}}|$base_image|g" Dockerfile.template > Dockerfile.$arch
+            docker build -t $tag_arch -f Dockerfile.$arch .
+    fi
+done
--- sha_function.sh
+++ sha_function.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+get_manifest_sha (){
+    local repo=$1     # <source>/alpine:latest
+    local arch=$2     # arm
+    docker pull -q $repo &>/dev/null
+    docker manifest inspect $repo > "$arch".txt
+    sha=""
+    local i=0
+    while [ "$sha" == "" ] && read -r line
+    do
+        archecture=$(jq .manifests[$i].platform.architecture "$arch".txt |sed -e 's/^"//' -e 's/"$//')
+        if [ "$archecture" = "$arch" ];then
+            sha=$(jq .manifests[$i].digest "$arch".txt  |sed -e 's/^"//' -e 's/"$//')
+            echo ${sha}
+        fi
+        i=$i+1
+    done < "$arch".txt
+}
+
 get_sha(){
-    repo=$1
     docker pull $1 &>/dev/null
-    #sha=$(docker image inspect $1 |jq .[0].RootFS.Layers |grep sha)
     sha=$(docker image inspect $1 | jq --raw-output '.[0].RootFS.Layers|.[]')   # [0] means first element of list,[]means all the elments of lists
     echo $sha
 }
@@ -11,22 +27,21 @@ get_sha(){
 is_base (){
     local base_sha    # alpine
     local image_sha   # new image
-    base_repo=$1
-    image_repo=$2
-    base_sha=$(get_sha $1)
-    image_sha=$(get_sha $2)
+    local base_repo=$1
+    local image_repo=$2
 
-    found="true"
+    base_sha=$(get_sha $base_repo)
+    image_sha=$(get_sha $image_repo)
+
     for i in $base_sha; do
         for j in $image_sha; do
             if [[ $i = $j ]]; then
-                #echo "no change, same base image: $i"
-                found="false"
-                break
+                echo "false"    #same base image
+                return 0
             fi
         done
     done
-    echo "$found"
+    echo "true"
 }
 
 get_service_version(){
@@ -40,10 +55,12 @@ get_service_version(){
 }
 
 compare (){
-    result=$(is_base $1 $2)
-    version1=$(get_service_version $3)
-    version2=$(get_service_version $4)
-    if [ $result == "true" ] || [ "$version1" != "$version2" ]; #compare alpine and service versions
+    result_arm=$(is_base $1 $2)
+    result_arm64=$(is_base $3 $4)
+    result_amd64=$(is_base $5 $6)
+    version1=$(get_service_version $7) #current on the docker hub (latest)
+    version2=$(get_service_version $8) #tag-amd64 newly built
+    if [ $result_arm == "true" ] || [ $result_amd64 == "true" ] || [ $result_arm64 == "true" ] || [ "$version1" != "$version2" ];     #compare alpine and service versions
     then
         echo "true"
     else
@@ -52,18 +69,19 @@ compare (){
 }
 
 create_manifest (){
-    local repo=$1 #treehouses/webssh
-    local tag_latest=$2 #latest
-    local tag_time=$3 #timetag
-    local tag_arm=$4  #treehouses/webssh-tags:arm
+    local repo=$1           #treehouses/webssh
+    local tag_latest=$2     #latest
+    local tag_time=$3       #timetag
+    local tag_arm=$4        #treehouses/webssh-tags:arm
     local tag_x86=$5
     local tag_arm64=$6
-    docker manifest create $repo:$tag_latest $tag_arm $tag_x86 $tag_arm64
-    docker manifest create $repo:$tag_time $tag_arm $tag_x86 $tag_arm64
-    docker manifest annotate $repo:$tag_latest $tag_arm --arch arm
-    docker manifest annotate $repo:$tag_latest $tag_x86 --arch amd64
+    docker manifest create   $repo:$tag_latest $tag_arm $tag_x86 $tag_arm64
+    docker manifest create   $repo:$tag_time   $tag_arm $tag_x86 $tag_arm64
+
+    docker manifest annotate $repo:$tag_latest $tag_arm   --arch arm
+    docker manifest annotate $repo:$tag_time   $tag_arm   --arch arm
+    docker manifest annotate $repo:$tag_latest $tag_x86   --arch amd64
+    docker manifest annotate $repo:$tag_time   $tag_x86   --arch amd64
     docker manifest annotate $repo:$tag_latest $tag_arm64 --arch arm64
-    docker manifest annotate $repo:$tag_time $tag_arm --arch arm
-    docker manifest annotate $repo:$tag_time $tag_x86 --arch amd64
-    docker manifest annotate $repo:$tag_time $tag_arm64 --arch arm64
+    docker manifest annotate $repo:$tag_time   $tag_arm64 --arch arm64
 }
