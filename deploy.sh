804d22f8e774cdac4c631cfa4b5f4b8a1e6d0cc0
fankaiyuan <172881969@qq.com>
2020-08-28 18:32:23 -0700

    update sha_function.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-for i in ${@}
-do
-    arch=$i
-    tag_arch=treehouses/webssh-tags:$arch
-    tag_time=$(date +%Y%m%d%H%M)
-    tag_arch_time=treehouses/webssh-tags:$arch-$tag_time
-    echo $tag_arch_time
-    docker tag $tag_arch $tag_arch_time
-    docker push $tag_arch_time
-    docker tag $tag_arch_time $tag_arch
-done
+arch=$1  # arm arm64 amd64
+tag_arch=treehouses/webssh-tags:$arch
+tag_time=$(date +%Y%m%d%H%M)
+tag_arch_time=treehouses/webssh-tags:$arch-$tag_time
+echo $tag_arch_time
 
-for i in ${@}
-do
-    arch=$i   # arm arm64 amd64
-    tag_arch=treehouses/webssh-tags:$arch
-    docker push $tag_arch
-done
+docker tag $tag_arch $tag_arch_time
+docker push $tag_arch_time
+docker tag $tag_arch_time $tag_arch
+docker push $tag_arch
